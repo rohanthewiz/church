48699e0323cb38840e8bc2d0d76266d5eccbeb7d
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2019-03-19 00:02:50 -0600

    Fix Payment Receipt link

--- resource/payment/payment_receipt.go
+++ resource/payment/payment_receipt.go
@@ -23,8 +23,9 @@ func (m ModulePaymentReceipt) Render(params map[string]map[string]string, logger
 	out = e("div", "class", "ch-module-wrapper ch-" + m.Opts.ModuleType).R(
 		e("h3", "class", "article-title").R("Thanks for your donation!"),
 		e("p", "class", "receipt-info").R(
-			"Your receipt is available here:",
-			m.Opts.Meta, "<br>Please save a copy for your records",
+			"Your receipt is available",
+			e("a", "href", m.Opts.Meta, "target", "_blank").R(" here."),
+			"<br>Please save a copy for your records, and close the browser window when finished",
 		),
 	)
 	return
