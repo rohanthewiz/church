249bed2b4544776388388007c61eb2d9ae8f2252
Rohan Allison <rohanthewiz@gmail.com>
2022-07-31 01:45:35 -0500

    Sermons upload fix

--- sermon_controller/sermon_controller.go
+++ sermon_controller/sermon_controller.go
@@ -4,6 +4,12 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+	"time"
+
 	"github.com/labstack/echo"
 	"github.com/rohanthewiz/church/app"
 	base "github.com/rohanthewiz/church/basectlr"
@@ -15,11 +21,6 @@ import (
 	"github.com/rohanthewiz/church/resource/sermon"
 	"github.com/rohanthewiz/church/template"
 	"github.com/rohanthewiz/logger"
-	"io"
-	"os"
-	"path"
-	"strings"
-	"time"
 )
 
 func NewSermon(c echo.Context) error {
@@ -84,14 +85,14 @@ func EditSermon(c echo.Context) error {
 func UpsertSermon(c echo.Context) error {
 	const sermonsLocalFilePrefix = "sermons"
 	const sermonsLocalURLPrefix = "media"
-	const ftpUploadDelay = time.Second * 40
+	const ftpUploadDelay = time.Second * 30
 
 	var fileUploaded bool
 	var localFilePath string
 	csrf := c.FormValue("csrf")
 	// Check that this token is present and valid in Redis
 	if !app.VerifyFormToken(csrf) {
-		err := errors.New("Your form is expired. Go back to the form, refresh the page and try again")
+		err := errors.New("your form is expired. Go back to the form, refresh the page and try again")
 		c.Error(err)
 		return err
 	}
@@ -106,22 +107,23 @@ func UpsertSermon(c echo.Context) error {
 	serPres.DateTaught = c.FormValue("sermon_date")
 	serPres.PlaceTaught = c.FormValue("sermon_place")
 	serPres.Teacher = c.FormValue("pastor-teacher")
-
 	sermonAudio, err := c.FormFile("sermon_audio")
-	//fmt.Printf("|** %#v\n", sermonAudio)
-	if err == nil && sermonAudio != nil && sermonAudio.Filename != "" {
+	sermonFilename := sermonAudio.Filename // get filename by value, not ptr to form data stuff
+
+	// fmt.Printf("|** %#v\n", sermonAudio)
+	if err == nil && sermonAudio != nil && sermonFilename != "" {
 		sermonTmp, err := sermonAudio.Open() // Todo: move to sermon model
 		if err != nil {
-			logger.LogErrAsync(err, "when", "opening sermon from FormFile", "filename", sermonAudio.Filename)
+			logger.LogErrAsync(err, "when", "opening sermon from FormFile", "filename", sermonFilename)
 			c.Error(err)
 			return err
 		}
 		defer sermonTmp.Close()
-		localFilePath = path.Join(sermonsLocalFilePrefix, sermonAudio.Filename)
-		initialUrlPath := path.Join(sermonsLocalURLPrefix, sermonAudio.Filename)
+		localFilePath = path.Join(sermonsLocalFilePrefix, sermonFilename)
+		initialUrlPath := path.Join(sermonsLocalURLPrefix, sermonFilename)
 		dest, err := os.Create(localFilePath)
 		if err != nil {
-			logger.LogErrAsync(err, "when", "creating destination file for sermon", "filename", sermonAudio.Filename)
+			logger.LogErrAsync(err, "when", "creating destination file for sermon", "filename", sermonFilename)
 			c.Error(err)
 			return err
 		}
@@ -130,7 +132,7 @@ func UpsertSermon(c echo.Context) error {
 
 		// Copy to server
 		if _, err := io.Copy(dest, sermonTmp); err != nil {
-			logger.LogErrAsync(err, "when", "copying sermon from FormFile to dest", "filename", sermonAudio.Filename)
+			logger.LogErrAsync(err, "when", "copying sermon from FormFile to dest", "filename", sermonFilename)
 			c.Error(err)
 			return err
 		}
@@ -151,7 +153,7 @@ func UpsertSermon(c echo.Context) error {
 	if c.FormValue("published") == "on" {
 		serPres.Published = true
 	}
-	//fmt.Printf("*|* serPres --> %#v\n", serPres)
+
 	slug, err := serPres.Upsert()
 	if err != nil {
 		c.Error(err)
@@ -165,7 +167,13 @@ func UpsertSermon(c echo.Context) error {
 	if config.Options.FTP.Main.Enabled && fileUploaded { // Transfer to main sermon archive
 		go func() {
 			time.Sleep(ftpUploadDelay)
-			upl := chftp.NewCemaUploader(localFilePath, sermonAudio.Filename, serPres.DateTaught)
+
+			logger.Log("Info", "Attempting xfer to FTP Server",
+				"localFilePath", localFilePath,
+				"sermonFilename", sermonFilename, "date taught", serPres.DateTaught,
+			)
+
+			upl := chftp.NewCemaUploader(localFilePath, sermonFilename, serPres.DateTaught)
 			println("Transferring", localFilePath, "to Main FTP server")
 			err := upl.Run()
 			if err != nil {
